eb31f455756ffdec8b421aedafc3ccc1567f2bf2
Sebastian Riedel <sri@cpan.org>
2016-12-09 11:23:38 +0100

    handle renamed files correctly

--- 50-tools/desktop-files-update.sh
+++ 50-tools/desktop-files-update.sh
@@ -19,7 +19,7 @@ mkdir desktopfiles
 cd desktopfiles
 count=1
 cat ../desktop-files-update.urls | while read url; do
-   case "$url" in 
+   case "$url" in
     http*)
       ../desktop-files-list.sh "$url" $count
       count=$(($count+1))
@@ -57,9 +57,9 @@ potlist=`cd 50-pot && ls -1 update-desktop-files*.pot | sed -e 's,.pot,,'`
 popd
 cd po
 incorrect=0
-svnlangs=`cd $podir && ls -1 */po/update-desktop-files*.po`
+svnlangs=`cd $podir && ls -1 */update-desktop-files*.po`
 for i in $svnlangs; do
-   i=`dirname $i | sed -e "s,/po,,"`
+   i=`dirname $i`
    if ! test -f $i/entries.po; then
       echo "Language $i is not collected"
       incorrect=1
@@ -74,8 +74,8 @@ fi
 for i in $lang/entries.po; do
       echo $i
       ilang=`echo $i | sed -e "s,/.*,,"`
-      opodir=$podir/$ilang/po
-      ofile=$opodir/update-desktop-files-all.$ilang.po
+      opodir=$podir/$ilang
+      ofile=$opodir/update-desktop-files-all.po
       # skip if there is no corresponing file in lcn
       [ -d $ilang ] || {
         echo "skipping \"$ilang\"; does not exist in lcn"
@@ -85,24 +85,24 @@ for i in $lang/entries.po; do
       # conflicts with active translators
       $svn up $opodir
       for f in $potlist; do
-        msgcat --no-wrap $podir/$ilang/po/$f.$ilang.po | \
+        msgcat --no-wrap $podir/$ilang/$f.po | \
           sed -e 's,msgid "",msgid "HEADER",' | \
           msggrep -K -E -e 'HEADER' - | \
           sed -e "s,HEADER,," > $f.header
       done
       # create update-desktop-files*po files (initializing)
       if [ -n "$init" ]; then
-        [ -f $podir/head-info.$ilang.po ] \
-          || { echo "template missing: $podir/head-info.$ilang.po"; exit 1; }
+        [ -f $podir/head-info.po ] \
+          || { echo "template missing: $podir/head-info.po"; exit 1; }
         for f in $podir/50-pot/update-desktop-files*.pot; do
-          pof=${f##*/}; pof=${pof%pot}$ilang.po
-          [ -f $podir/$ilang/po/$pof ] \
-            || cp $podir/head-info.$ilang.po $podir/$ilang/po/$pof
+          pof=${f##*/}; pof=${pof%pot}.po
+          [ -f $podir/$ilang/$pof ] \
+            || cp $podir/head-info.po $podir/$ilang/$pof
         done
       fi
       # use --force-po otherwise initializing fails
       msgcat --force-po --use-first -o - \
-        $podir/$ilang/po/update-desktop-files*.$lang.po \
+        $podir/$ilang/update-desktop-files*.po \
         | msgattrib --force-po --translated --no-fuzzy -s -o $ofile
       msgfmt --check -o /dev/null $ofile || {
         echo "broken $ofile" | tee -a desktop.log
@@ -116,15 +116,15 @@ for i in $lang/entries.po; do
       fi
       msgcat --more-than=1 -o - $i $ofile | msgattrib --only-fuzzy --no-wrap |\
 	   sed -e 's,#-#-#-#-#  update-desktop-files-all.*,SVN:\\n\",; s,#-#-#-#-#  entries.*,Packages:\\n\",' |\
-	   msgcat -s -o $podir/$ilang/po/update-desktop-files-conflicts.$ilang.po  -
+	   msgcat -s -o $podir/$ilang/update-desktop-files-conflicts.po  -
       msgcat -o new.po --use-first $i $ofile
       msgmerge -s -C $ofile -o new.po new.po new.po
       for f in $potlist; do
          msgmerge --previous -s -o - new.po $podir/50-pot/$f.pot | LC_ALL=C grep -v '^#~' | uniq | \
-            msgcat -s -o $podir/$ilang/po/$f.$ilang.po  --use-first $f.header -
+            msgcat -s -o $podir/$ilang/$f.po  --use-first $f.header -
          rm $f.header
-         msgmerge --previous -s -o $podir/$ilang/po/$f.$ilang.po $podir/$ilang/po/$f.$ilang.po $podir/50-pot/$f.pot
-         echo $ilang/po/$f.$ilang.po
+         msgmerge --previous -s -o $podir/$ilang/$f.po $podir/$ilang/$f.po $podir/50-pot/$f.pot
+         echo $ilang/$f.po
       done
       rm $ofile
       $svn add $opodir/update-desktop-files*.po
